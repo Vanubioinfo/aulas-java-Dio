1d1448867dd2ffc69cf4ceeb12ca2130356b8c0f
vanubioinfo <vanubio06@gmail.com>
2022-10-14 21:28:44 -0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,53 @@
+
+
+//"Exrecitando conceituação e criação de variaveis."
+
+public class main {
+
+    public static void main(String[] args){
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $ap;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $ap = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn246_78md = 10;
+        //int asrn2$46%78_md = 10;
+
+        asrn24678md = 100;
+        //asrn24678_md = 10;
+
+        int quantidadedeProduto = 50;
+        int QuantodadedeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUNATIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($ap);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn246_78md);
+
+        System.out.println(quantidadedeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUNATIDADE_OPCOES);
+
+
+    }
+
+}
